3ef5abca057980ab6f47df59da23fe89369e3252
Nelther GP <z44.nelther@gmail.com>
2015-05-17 20:03:41 -0500

    mas inferencia

--- SistemaExperto/src/sistemaexperto/Inferencia.java
+++ SistemaExperto/src/sistemaexperto/Inferencia.java
@@ -8,6 +8,7 @@ package sistemaexperto;
 import contenedor.*;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.RandomAccess;
@@ -19,108 +20,146 @@ import java.util.logging.Logger;
  * @author nelther
  */
 public class Inferencia {
-    public Queue <Integer> conjuntoConflicto = new LinkedList <Integer>();
+
+    public Queue<Integer> conjuntoConflicto = new LinkedList<Integer>();
+    public ArrayList <Integer> reglasVisitadas= new ArrayList<Integer>();
     public FileManager manager = new FileManager();
-    public RandomAccessFile BaseConocimientos,indice,BaseHechos;
-    int tamañoAnt_Con=40;
-    int tamañoLave=4;
-    int numAnt=5;
-    String Urlmaestro="archivos/maestro";
-    String Urlindice="archivos/indice";
-    String UrlBaseHechos="archivos/hechos";
-    
+    public RandomAccessFile BaseConocimientos, indice, BaseHechos;
+    int tamañoAnt_Con = 40;
+    int tamañoLave = 4;
+    int numAnt = 5;
+    String Urlmaestro = "archivos/maestro";
+    String Urlindice = "archivos/indice";
+    String UrlBaseHechos = "archivos/hechos";
+
     public Arbol tree = new Arbol();
-    
-    public Inferencia() throws IOException{
-      try {
-        indice=manager.Abrir(Urlindice);
-        BaseHechos=manager.Abrir(UrlBaseHechos);
-        BaseConocimientos=manager.Abrir(Urlmaestro);
-      } catch (IOException ex) {
-        Logger.getLogger(Inferencia.class.getName()).log(Level.SEVERE, null, ex);
-      }
-        llenarArbol();
-        equiparacion();
-        BaseHechos.close();
-    }
-     
-    public void equiparacion () throws IOException{
-     preOrden(tree.raiz);
-        if(conjuntoConflicto.isEmpty()){
-           System.out.println("CC vacio");
-       }else{
-        
-        while(!conjuntoConflicto.isEmpty()){
-            System.out.println(conjuntoConflicto.poll());
-        }
-       }
-    }
-    
-    public void resolucion(){
-        
-    }
-    
-    public void encadenamientoProgresivo(){
-   
-    }
-    public void llenarArbol(){
-        try { 
-            while(indice.getFilePointer()<indice.length()){
-                System.out.println("llenando arbol");
-                tree.insertart(indice.readInt(), indice.readInt());
-            }   
+
+    public Inferencia() throws IOException {
+        try {
+            indice = manager.Abrir(Urlindice);
+            BaseHechos = manager.Abrir(UrlBaseHechos);
+            BaseConocimientos = manager.Abrir(Urlmaestro);
         } catch (IOException ex) {
             Logger.getLogger(Inferencia.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+        encadenamientoProgresivo();
+        BaseHechos.close();
     }
-    
-    public void preOrden(Nodo raiz){
-        Nodo l,d;
-        if(raiz!=null){
-           procesar(raiz);
-           l=raiz.izq;
-           preOrden(l);
-           d=raiz.der;
-           preOrden(d);
-       }
-}
-    
-    public void procesar(Nodo raiz){
-        boolean flag=false;
-        System.out.println("Equiparando Regla: "+raiz.llave+","+raiz.pos);
-        try {
-            BaseHechos.seek(0);
-            while(BaseHechos.getFilePointer()<BaseHechos.length()){
-                
-                       String fact=manager.formar_string(BaseHechos, 20);
-                       System.out.println("Hecho Eq= "+fact);
-                       for(int i=0;i<numAnt;i++){
-                           if(i==0){
-                              BaseConocimientos.seek(raiz.pos+tamañoLave+((tamañoAnt_Con*5)+8)+(i*tamañoAnt_Con));
-                           }else{
-                               BaseConocimientos.seek(raiz.pos+tamañoLave+((tamañoAnt_Con*5)+8)+(i*tamañoAnt_Con)+2);
-                           }
-                           //System.out.println("apuntador BC: "+BaseConocimientos.getFilePointer());
-                           String ant=manager.formar_string(BaseConocimientos, 20);
-                           
-                            if(ant.length()!=0){
-                                System.out.println("Antecedente Eq= "+ant+","+ant.length());
-                                if(fact.equalsIgnoreCase(ant)){
-                                      flag=true;
-                                     //System.out.println(flag);
-                                  
-                                 }else{
-                                    //System.out.println(flag);
-                                      flag=false;
-                                 }   
-                            }
-                       }
-                       if(flag){
-                           conjuntoConflicto.add(raiz.llave);
-                       }
-                                     
+
+    public void equiparacion() throws IOException {
+        preOrden(tree.raiz);
+        if (conjuntoConflicto.isEmpty()) {
+            System.out.println("CC vacio");
+        } else {
+            while (!conjuntoConflicto.isEmpty()) {
+                System.out.println(conjuntoConflicto.poll());
             }
+        }
+    }
+
+    public boolean resolucion(Queue <Integer> cc) {
+        boolean alcanzado = false;
+        if(!cc.isEmpty()){
+           
+            try {
+                BaseConocimientos.seek(cc.peek());
+                String nuevoHecho=manager.formar_string(BaseConocimientos, tamañoAnt_Con);
+                BaseHechos.seek(0);
+                while(BaseHechos.getFilePointer()<BaseHechos.length()){
+                    if(nuevoHecho.equalsIgnoreCase(manager.formar_string(BaseHechos, tamañoAnt_Con))){
+                        alcanzado=true;
+                    }else{
+                        alcanzado=false;
+                    }
+                }
+                if(!alcanzado){
+                    BaseHechos.seek(BaseHechos.length());
+                    manager.m_EscribirString(nuevoHecho, numAnt, indice);
+                }
+                
+            } catch (IOException ex) {
+                Logger.getLogger(Inferencia.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
+        }
+        return alcanzado;
+    }
+
+    public void encadenamientoProgresivo() {
+        try {
+            llenarArbol();
+            do{
+                equiparacion();
+            }
+            while(!resolucion(conjuntoConflicto));
+            BaseHechos.seek(BaseConocimientos.length()-tamañoAnt_Con);
+            String meta = manager.formar_string(BaseHechos, tamañoAnt_Con);
+            System.out.println("Resultado: "+meta);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Inferencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void llenarArbol() {
+        try {
+            while (indice.getFilePointer() < indice.length()) {
+                System.out.println("llenando arbol");
+                tree.insertart(indice.readInt(), indice.readInt());
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Inferencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void preOrden(Nodo raiz) {
+        Nodo l, d;
+        
+        if (raiz != null) {
+            procesar(raiz);
+            l = raiz.izq;
+            preOrden(l);
+            d = raiz.der;
+            preOrden(d);
+        }
+    }
+
+    public void procesar(Nodo raiz) {
+        boolean flag = false;
+        System.out.println("Equiparando Regla: " + raiz.llave + "," + raiz.pos);
+        try {
+            for (int i = 0; i < 5; i++) {
+                
+                if (i == 0) { //Coloca el apuntador en el antecedente correspondiente
+                    BaseConocimientos.seek(raiz.pos + tamañoLave + ((tamañoAnt_Con * 5) + 8) + (i * tamañoAnt_Con));
+                } else { //Coloca el apuntador en el antecedente correspondiente si no es el primero salta el operador
+                    BaseConocimientos.seek(raiz.pos + tamañoLave + ((tamañoAnt_Con * 5) + 8) + (i * tamañoAnt_Con) + 2);
+                }
+                String ant = manager.formar_string(BaseConocimientos, 20);
+                BaseHechos.seek(0);//coloca el apuntador en el inicio de la base de hechos
+                                   //Para cada antecedente debe leerse toda la base de hechos
+                while (BaseHechos.getFilePointer() < BaseHechos.length()) {
+                    String fact = manager.formar_string(BaseHechos, 20);
+                   
+                    if (ant.length() != 0) {
+                        System.out.println("Hecho: "+fact+" Ant: "+ant);
+                        if (fact.equalsIgnoreCase(ant)) {
+                            flag = true;
+                            break;
+                            //System.out.println(flag);
+                        } else {
+                            //System.out.println(flag);
+                            flag = false;
+                        }
+                    }
+                }
+                if(!flag){
+                    break;
+                }
+            }if(flag){
+                        conjuntoConflicto.add(raiz.pos);
+                    }
         } catch (IOException ex) {
             Logger.getLogger(Inferencia.class.getName()).log(Level.SEVERE, null, ex);
         }
